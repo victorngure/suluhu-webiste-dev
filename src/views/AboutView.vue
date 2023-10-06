c27753d248a344cdb2930b4501b985ce64e0e9e5
devrobintel <devrobertkibugi@gmail.com>
2023-10-06 14:43:05 +0300

    added google analytics code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,13 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D5ETE99HJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D5ETE99HJY');
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -290,6 +290,16 @@
   </div>
 </template>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D5ETE99HJY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D5ETE99HJY');
+</script>
+
 <script>
 import TopNavComponent from '@/components/TopNavComponent.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
